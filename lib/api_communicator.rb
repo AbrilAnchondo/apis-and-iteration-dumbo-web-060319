4445534c8b6494903af92c5fcf41ad028d13b3da
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-04 12:38:52 -0400

    nothing

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,12 +6,14 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+ 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
+
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
@@ -26,6 +28,9 @@ def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
+
+
+
 
 ## BONUS
 
