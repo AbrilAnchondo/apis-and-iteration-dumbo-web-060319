2610ae3abeaf7e1d99291db930115376e3a10ce9
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-04 15:29:58 -0400

    Completed

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -31,12 +31,6 @@ end
 
 
 
-<<<<<<< HEAD
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-   
-=======
   films = response_hash["results"][0]["films"]
   films.each do |film_url|
     result << film_api_helper(film_url) 
@@ -47,7 +41,6 @@ end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
 
->>>>>>> 3e5c676b817f77775e91caf5647a3ea0109c0bac
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   
@@ -59,18 +52,6 @@ end
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-<<<<<<< HEAD
- 
-
-
-def print_movies(films)
- # some iteration magic and puts out the movies in a nice list
-  films.each do |film|
-    response_film = RestClient.get(film)
-    response_film_hash = JSON.parse(response_film)
-    puts "Title: #{response_film_hash["title"]} Description: #{response_film_hash[opening-crawl]}"
-  end
-=======
 
 end
 
@@ -86,7 +67,6 @@ def print_movies(films)
     puts ""
   end
 
->>>>>>> 3e5c676b817f77775e91caf5647a3ea0109c0bac
 end
 
 def show_character_movies(character)
@@ -94,31 +74,14 @@ def show_character_movies(character)
   print_movies(films)
 end
 
-<<<<<<< HEAD
- 
-
-#get_character_movies_from_api("samip")
-=======
->>>>>>> 3e5c676b817f77775e91caf5647a3ea0109c0bac
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
 
-<<<<<<< HEAD
-#def film_api_helper(url)
-
-#response_string=RestClient.get url
-#response_hash=JSON.parse(response_string)
-#end
-
-  #film_api_helper("https://www.swapi.co/api/films/2/")
-  #[{}]
-=======
 
 
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
->>>>>>> 3e5c676b817f77775e91caf5647a3ea0109c0bac
